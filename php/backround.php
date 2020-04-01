edd18a2885d93f34946439fb8e9f7b45f23b03fd
LM <lmleflochb@gmail.com>
2020-04-01 20:46:15 +0200

    optimisation de backround.php

--- php/backround.php
+++ php/backround.php
@@ -3,15 +3,8 @@
 if (!isset($_SESSION['NbWal'])) {
     $_SESSION['NbWal'] = random_int(0, 2);
 }
-switch ($_SESSION['NbWal']) {
-    case 0:
-        echo "<style type=text/css>body {background-image: url(../image/insert_backround0.jpg);}</style>";
-        break;
-    case 1:
-        echo "<style type=text/css>body {background-image: url(../image/insert_backround1.jpg);}</style>";
-        break;
-    case 2:
-        echo "<style type=text/css>body {background-image: url(../image/insert_backround2.jpg);}</style>";
-        break;
-}
+
+
+echo "<style type=text/css>body {background-image: url(../image/insert_backround".$_SESSION['NbWal'].".jpg);}</style>";
+
 ?>
