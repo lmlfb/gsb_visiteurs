28e4625edd0c3f40d91a5c3ccb18ed82c592861e
LM <lmleflochb@gmail.com>
2020-04-01 22:58:00 +0200

    imageUpdate

--- php/backround.php
+++ php/backround.php
@@ -1,10 +1,11 @@
 <?php
 //pas besoin de demarrarer un session car toujours appelé après lancement dans autres php
 if (!isset($_SESSION['NbWal'])) {
-    $_SESSION['NbWal'] = random_int(0, 2);
+    $_SESSION['NbWal'] = random_int(0, 6);
 }
 
 
 echo "<style type=text/css>body {background-image: url(../image/insert_backround".$_SESSION['NbWal'].".jpg);}</style>";
+
 
 ?>
